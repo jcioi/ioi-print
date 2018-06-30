7268be35e14c8b0df471f5a9e8dfd91dde93432b
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-06-30 15:45:42 +0900

    Add site-admin stub

---
+++ stub-admin-api/api.rb
@@ -0,0 +1,47 @@
+require 'sinatra/base'
+require 'json'
+
+class AdminAPI < Sinatra::Base
+  get '/api/nodes/ip/:ip/' do
+    [
+      200,
+      {Rack::CONTENT_TYPE => 'application/json'},
+      [JSON.dump(node_data(params['ip']))],
+    ]
+  end
+
+  get '/api/nodes/ip/:ip/map/' do
+    [
+      200,
+      {Rack::CONTENT_TYPE => 'image/svg+xml'},
+      [map_data(params['ip'])],
+    ]
+  end
+
+  private
+
+  def node_data(ip)
+    {
+      node: {
+        ip: ip,
+        mac: '00:00:00:00:00:00',
+      },
+      contestant: {
+        id: 'JPN-345',
+        country: 'JPN',
+        number: 345,
+        name: 'Morumotto Reloaded',
+      },
+      desk: {
+        zone: 'A',
+        number: 10,
+        map: "/api/nodes/ip/#{ip}/map/",
+        id: 'A10',
+      },
+    }
+  end
+
+  def map_data(ip)
+    IO.read('assets/desk_map.svg')
+  end
+end
