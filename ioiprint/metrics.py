f34384c195eba1e068aeee04b54583940d5a337c
tyage <namatyage@gmail.com>
2018-08-31 13:27:43 +0900

    use Redis instead of StrictRedis

--- ioiprint/metrics.py
+++ ioiprint/metrics.py
@@ -1,7 +1,7 @@
 import redis
 
 print_count_prefix = 'print_count:'
-db = redis.strictRedis(host='redis', port=6379)
+db = redis.Redis(host='redis', port=6379)
 
 def countup_print(job_name):
   key = print_count_prefix + job_name
