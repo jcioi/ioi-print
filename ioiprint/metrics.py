9c29f503f92929304815e0e709f5240388a10dee
tyage <namatyage@gmail.com>
2018-08-31 13:00:10 +0900

    count print

---
+++ ioiprint/metrics.py
@@ -0,0 +1,8 @@
+import redis
+
+print_count_prefix = 'print_count:'
+db = redis.strictRedis(host='redis', port=6379)
+
+def countup_print(job_name):
+  key = print_count_prefix + job_name
+  db.incr(key)
--- ioiprint/print.py
+++ ioiprint/print.py
@@ -1,11 +1,14 @@
 import subprocess
 
 from ioiprint.settings import CUPS_SERVER_ADDRESS
+from ioiprint.metrics import countup_print
 
 JOB_PRIORITY_DEFAULT=1
 JOB_PRIORITY_HIGH=50
 
 def print_file(file_path, printer, job_name=None, job_priority=JOB_PRIORITY_DEFAULT):
+    countup_print(job_name)
+
     args = ['-H', CUPS_SERVER_ADDRESS, '-P', printer]
     if job_name is not None:
         args.extend(['-J', job_name])
