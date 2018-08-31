bb5b5c23ce0e02ba8ddfe03a45d9c3700a34645a
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-09-01 01:29:16 +0900

    Revert "Print metrics"

--- ioiprint/print.py
+++ ioiprint/print.py
@@ -1,16 +1,11 @@
 import subprocess
 
 from ioiprint.settings import CUPS_SERVER_ADDRESS
-from ioiprint.metrics import countup_print
 
 JOB_PRIORITY_DEFAULT=1
 JOB_PRIORITY_HIGH=50
 
-def print_file(file_path, printer, job_name=None, job_priority=JOB_PRIORITY_DEFAULT, metric_name=None):
-    if metric_name == None:
-        metric_name = job_name
-    countup_print(metric_name)
-
+def print_file(file_path, printer, job_name=None, job_priority=JOB_PRIORITY_DEFAULT):
     args = ['-H', CUPS_SERVER_ADDRESS, '-P', printer]
     if job_name is not None:
         args.extend(['-J', job_name])
