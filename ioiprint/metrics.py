3afb143bba020dd852230efe4737fa96dc87a448
tyage <namatyage@gmail.com>
2018-09-01 12:22:30 +0900

    fix uninitialized bug

--- ioiprint/metrics.py
+++ ioiprint/metrics.py
@@ -45,6 +45,8 @@ def get_metrics():
     metrics = load_metrics()
     for key, count in metrics.items():
         job_type = search_job_type_from_key(key)
+        if job_type not in counter:
+            counte[job_type] = {}
         job_value = get_job_value_from_key(job_type, key)
         counter[job_type][job_value] = count
     return counter
